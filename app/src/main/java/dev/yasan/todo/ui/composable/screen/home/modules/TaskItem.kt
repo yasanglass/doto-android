8edcd14ca35008306e3273ea33f0717e5fb0331b
YASAN <yasandev@gmail.com>
2022-01-20 01:36:54 +0330

    Update TaskItem.kt

--- app/src/main/java/dev/yasan/todo/ui/composable/screen/home/modules/TaskItem.kt
+++ app/src/main/java/dev/yasan/todo/ui/composable/screen/home/modules/TaskItem.kt
@@ -4,8 +4,11 @@ import android.content.res.Configuration
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Divider
+import androidx.compose.material.Icon
 import androidx.compose.material.Text
+import androidx.compose.material.icons.sharp.Description
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
@@ -19,6 +22,7 @@ import dev.yasan.todo.R
 import dev.yasan.todo.data.db.entity.Task
 import dev.yasan.todo.ui.preview.TaskListPreviewProvider
 import dev.yasan.todo.ui.preview.TaskPreviewProvider
+import dev.yasan.todo.ui.theme.DoToIcons
 import dev.yasan.todo.ui.theme.dimenDivider
 import dev.yasan.todo.ui.theme.grid
 import dev.yasan.todo.ui.theme.rubikFamily
@@ -36,23 +40,35 @@ fun TaskItem(
             .fillMaxWidth()
             .background(color = colorResource(id = R.color.layer_foreground))
     ) {
-        Column(modifier = Modifier.padding(horizontal = grid(2), vertical = grid(1.5f))) {
-            Text(
-                text = task.title,
-                color = colorResource(id = R.color.text_title),
-                fontFamily = fontFamily,
-                fontSize = 18.sp,
-                fontWeight = FontWeight.Bold
-            )
-            Spacer(modifier = Modifier.requiredHeight(grid()))
-            Text(
-                text = stringResource(id = R.string.created_on, task.creationDatString),
-                color = colorResource(id = R.color.text_desc),
-                fontFamily = fontFamily,
-                fontSize = 10.sp,
-                fontWeight = FontWeight.Light,
-                fontStyle = FontStyle.Italic
-            )
+        Row(
+            modifier = Modifier.padding(horizontal = grid(2), vertical = grid(1.5f)),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Column(modifier = Modifier.weight(1f)) {
+                Text(
+                    text = task.title,
+                    color = colorResource(id = R.color.text_title),
+                    fontFamily = fontFamily,
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.Bold
+                )
+                Spacer(modifier = Modifier.requiredHeight(grid()))
+                Text(
+                    text = stringResource(id = R.string.created_on, task.creationDatString),
+                    color = colorResource(id = R.color.text_desc),
+                    fontFamily = fontFamily,
+                    fontSize = 10.sp,
+                    fontWeight = FontWeight.Light,
+                    fontStyle = FontStyle.Italic
+                )
+            }
+            if (!task.description.isNullOrBlank()) {
+                Icon(
+                    imageVector = DoToIcons.Description,
+                    contentDescription = stringResource(id = R.string.description),
+                    tint = colorResource(id = R.color.text_desc)
+                )
+            }
         }
         Divider(color = colorResource(id = R.color.divider), thickness = dimenDivider)
     }
